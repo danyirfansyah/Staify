be4075d83329403eed341c02e5f33c31c6512d5b
danyirfansyah <danyirfansyah23.com>
2024-05-06 22:27:04 +0700

    initial commit

---
+++ db_connection.php
@@ -0,0 +1,15 @@
+<?php
+// Replace the database credentials with your own
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
